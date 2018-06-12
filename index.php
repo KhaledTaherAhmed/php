8309ea0d3eb51931558966995f7e4cfcbe13b102
Khaled <Khaledtaherahmed@gmail.com>
2018-06-12 19:53:04 +0200

    adding index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo ("This is a proxy server.") ;
+echo ("It works as intermediary between the arduino and the firebase. ") ;
+
+echo ("The data is accessed from the path ./script?data...") ;
+
+?>
